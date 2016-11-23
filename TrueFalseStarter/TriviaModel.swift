bb11bd1a807b9354157880966518f5980f2ef431
Nathan Rohweder <Rohwedernt@Nathans-MacBook-Pro.local>
2016-11-22 20:04:21 -0500

    Created TriviaModel.swift Implemented struct with to hold collection of trivia data Updated references in ViewController

---
+++ TrueFalseStarter/TriviaModel.swift
@@ -0,0 +1,16 @@
+//
+//  TriviaModel.swift
+//  TrueFalseStarter
+//
+//  Created by Nathan Rohweder on 11/22/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+struct TriviaModel {
+    let trivia: [[String : String]] = [
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+}
